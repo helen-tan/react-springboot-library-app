2e968b848e97d348cd1afd8079ae0559b6d10093
helen-tan <helentanmz0909@gmail.com>
2023-02-13 16:49:55 +0800

    Setup Java Spring Boot backend with dependencies

---
+++ backend/react-springboot-library/src/main/java/com/luv2code/reactspringbootlibrary/ReactSpringbootLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.reactspringbootlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootLibraryApplication.class, args);
+	}
+
+}
---
+++ backend/react-springboot-library/src/test/java/com/luv2code/reactspringbootlibrary/ReactSpringbootLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.reactspringbootlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
