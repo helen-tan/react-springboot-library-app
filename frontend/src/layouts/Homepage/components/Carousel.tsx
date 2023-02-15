98667dd6bfce65a5d01ea23c1a3e52098437121d
helen-tan <helentanmz0909@gmail.com>
2023-02-15 11:41:43 +0800

    Fetch books for carousel

--- frontend/src/layouts/Homepage/components/Carousel.tsx
+++ frontend/src/layouts/Homepage/components/Carousel.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { ReturnBook } from './ReturnBook'
-
+import BookModel from '../../../models/BookModel'
 
 export const Carousel = () => {
+    const [book, setBooks] = useState<BookModel[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const baseUrl: string = "http://localhost:8080/api/books";
+
+            const url: string = `${baseUrl}?page=0&size=9`;
+
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img
+                })
+            }
+
+            setBooks(loadedBooks);
+            setLoading(false);
+        };
+
+        fetchBooks().catch((error: any) => {
+            setLoading(false);
+            setHttpError(error.message)
+        })
+    }, [])
+
     return (
         <div className='container mt-5' style={{ height: 550 }}>
             <div className="homepage-carousel-title">
