045fb2d6d59ab1dbbc13daa4f68abec704782739
helen-tan <helentanmz0909@gmail.com>
2023-02-15 16:24:30 +0800

    SearchBooksPage elements

--- frontend/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ frontend/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -63,6 +63,67 @@ export const SearchBooksPage = () => {
     }
 
     return (
-        <div>SearchBooksPage</div>
+        <div>
+            <div className='container'>
+                <div>
+                    <div className='row mt-5'>
+                        <div className="col-6">
+                            <div className="d-flex">
+                                <input type="search" className='form-control me-2' placeholder='Search' aria-labelledby='Search' />
+                                <button className='btn btn-outline-success'>
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className='col-4'>
+                            <div className="dropdown">
+                                <button className='btn btn-secondary dropdown-toggle' type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Category
+                                </button>
+                                <ul className='dropdown-menu' aria-labelledby="dropdownMenuButton1">
+                                    <li>
+                                        <a href="#" className="dropdown-item">
+                                            All
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="dropdown-item">
+                                            Front End
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="dropdown-item">
+                                            Back End
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="dropdown-item">
+                                            Data
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="dropdown-item">
+                                            DevOps
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='mt-3'>
+                        <h5>Number of results: {22}</h5>
+                    </div>
+                    <p>
+                        1 to 5 of 22 items:
+                    </p>
+                    {books.map(book => (
+                        <div></div>
+                        // <SearchBook book={book} key={book.id}/>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
