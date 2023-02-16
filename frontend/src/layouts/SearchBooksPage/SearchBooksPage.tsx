9e5404e163ba1f233b65371f17348581cab8ec1f
helen-tan <helentanmz0909@gmail.com>
2023-02-16 11:50:25 +0800

    Search result text to render dynamically

--- frontend/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ frontend/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -131,10 +131,10 @@ export const SearchBooksPage = () => {
                     </div>
 
                     <div className='mt-3'>
-                        <h5>Number of results: {22}</h5>
+                        <h5>Number of results: {totalAmountOfBooks}</h5>
                     </div>
                     <p>
-                        1 to 5 of 22 items:
+                        <>{indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:</>
                     </p>
                     {books.map(book => (
                         <SearchBook book={book} key={book.id} />
