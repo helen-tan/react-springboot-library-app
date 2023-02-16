8f629ef564d3d7ee34b34eabcdc395ca4777720c
helen-tan <helentanmz0909@gmail.com>
2023-02-16 17:26:19 +0800

    Some routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/Homepage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Route, Routes } from 'react-router';
 
 export const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      {/* <HomePage/> */}
-      <SearchBooksPage/>
-      <Footer/>
-    </>
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchBooksPage />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
