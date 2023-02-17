5133a831de9c1a137a1df40c0018b7b9076d1643
helen-tan <helentanmz0909@gmail.com>
2023-02-17 15:00:35 +0800

    Changed a tag to react Link

--- frontend/src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ frontend/src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import BookModel from '../../../models/BookModel'
 
 export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
@@ -53,9 +54,9 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
                 </div>
 
                 <div className="col-md-4 d-flex justify-content-center align-items-center">
-                    <a href="#" className="btn btn-md main-color text-white">
+                    <Link to={`/checkout/${props.book.id}`} className="btn btn-md main-color text-white">
                         View Details
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
