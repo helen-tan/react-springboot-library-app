822996077584414c2e53f6df51e0cff7e5db8057
helen-tan <helentanmz0909@gmail.com>
2023-02-17 14:37:49 +0800

    BookCheckoutPage: Send req to get 1 book based on bookId

--- frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import BookModel from '../../models/BookModel'
+import { Spinner } from '../utils/Spinner';
 
 export const BookCheckoutPage = () => {
-  return (
-    <div >
-        <h3>Hi World!</h3>
-    </div>
-  )
+    const [book, setBook] = useState<BookModel>();
+    const [loading, setLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    const bookId = (window.location.pathname).split('/')[2];
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+            const response = await fetch(baseUrl);
+
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+
+            const responseJson = await response.json();
+
+            const loadedBook: BookModel = {
+                id: responseJson.id,
+                title: responseJson.title,
+                author: responseJson.author,
+                description: responseJson.description,
+                copies: responseJson.copies,
+                copiesAvailable: responseJson.copiesAvailable,
+                category: responseJson.category,
+                img: responseJson.img
+            };
+
+            setBook(loadedBook);
+            setLoading(false);
+        };
+
+        fetchBook().catch((error: any) => {
+            setLoading(false);
+            setHttpError(error.message)
+        })
+    }, [])
+
+    if (loading) {
+        return (
+            <Spinner />
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    return (
+        <div >
+            <h3>Hi World!</h3>
+        </div>
+    )
 }
